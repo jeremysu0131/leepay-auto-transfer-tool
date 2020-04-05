7bc10250627c66ab2416b2c82cc3b83fb123c680
Jeremy Su <>
2020-04-05 17:51:36 +0800

    Set ABC test case

---
+++ tests/unit/workers/ABCWorker.spec.ts
@@ -0,0 +1,21 @@
+import BankWorker from "@/workers/BankWorker";
+
+var worker: BankWorker;
+var remitterAccount = {
+  balance: 0,
+  code: "T.ABC.001",
+  loginName: "test001",
+  loginPassword: "testpsw",
+  usbPassword: "testpsw",
+  proxy: "10.9.8.7:8800"
+};
+
+beforeAll(() => {
+  worker = new BankWorker(remitterAccount);
+});
+
+describe("ABCWorker:launchSelenium", () => {
+  it("isLaunched", () => {
+    expect(worker.launchSelenium()).toBe(true);
+  });
+});
