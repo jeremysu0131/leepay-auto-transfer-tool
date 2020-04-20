87b22b3d3a126370c3508dc898e640ed94fb62c3
Jeremy Su <>
2020-04-20 12:49:28 +0800

    Set risk request

---
+++ src/utils/requestRisk.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const service = axios.create({
+  baseURL: process.env.VUE_BANK_RISK_URL,
+  timeout: 15000
+});
+
+// Request interceptors
+service.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+// Response interceptors
+service.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
+
+export default service;
