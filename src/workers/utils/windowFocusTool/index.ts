fedcaef5026b64be67968be3d73027db9dd75a23
charles.lin <charles.lin@tc-gaming.com>
2020-04-24 13:35:34 +0800

    修改 window_focus_tool 調用方式

--- src/workers/utils/windowFocusTool/index.ts
+++ src/workers/utils/windowFocusTool/index.ts
@@ -1,20 +1,11 @@
-import { execSync } from "child_process";
-import logger from "../logger";
+const { exec } = require("child_process");
+const cwd =
+  process.env.NODE_ENV === "production" ? process.resourcesPath + "/bankWorkerTool" : "bankWorkerTool";
 
 export function focusAndCheckIE() {
-  run();
-}
-
-function run() {
-  try {
-    execSync("BankWorkerTool.exe WINDOW_FOCUS_TOOL", {
-      // shell: false,
-      cwd:
-        process.env.NODE_ENV === "production"
-          ? process.resourcesPath + "/bankWorkerTool"
-          : "bankWorkerTool"
-    });
-  } catch (error) {
-    logger({ level: "error", message: error });
-  }
+  return new Promise((resolve, reject) => {
+    var child = exec("BankWorkerTool.exe \"WINDOW_FOCUS_TOOL\"", { shell: false, cwd });
+    child.on("error", (err: any) => reject(err));
+    child.on("exit", () => resolve());
+  });
 }
