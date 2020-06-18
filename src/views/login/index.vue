cf285a5042605fcdc4326d6208f8633c9728676c
jeremy.s <jeremy.s@tc-gaming.com>
2020-06-18 15:11:41 +0800

    Fix the error of login info

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -97,8 +97,8 @@ export default class extends Vue {
     }
   };
   private loginForm = {
-    username: "jeremy",
-    password: "1qaz@WSX"
+    username: process.env.NODE_ENV === "production" ? "" : "js",
+    password: process.env.NODE_ENV === "production" ? "" : "123456"
   };
   private loginRules = {
     username: [{ validator: this.validateUsername, trigger: "blur" }],
